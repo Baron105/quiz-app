db3af5ac12358e3d451398e9707adcf0956ffacf
Baron105 <barunparua@gmail.com>
2023-07-04 11:19:46 +0530

    adding the app

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # quiz-app
+
+Python Quiz App using questions from the open source [Open Trivia Database](https://opentdb.com/)
+
+## Requirements
+
+python3 and tkinter, requests, json, random, html, pyttsx3 modules and a working internet connection to fetch the questions from the database
+
+## Usage
+
+Run the following command in the terminal
+
+```python3 quiz.py```
+
+Enter the number of questions you want to answer, the difficulty level and the category of questions and click on start button
+
+Use the radio buttons to select the answers of the question and click on next button to go to the next question
+
+You may quit the quiz at any time by clicking on the quit button
+
+At the end of the quiz the score would be displayed
+
+## Note
+
+main.py is a CLI version and gui.py is a basic GUI version of quiz.py
+they are not required to run the quiz app
---
+++ quiz.py
@@ -0,0 +1,217 @@
+from tkinter import *
+from tkinter import messagebox as mb
+import json
+import html
+import requests
+from random import shuffle
+import pyttsx3
+
+engine = pyttsx3.init()
+
+def fetch_questions(amount=10, category=9, difficulty='easy', question_type='multiple'):
+    url = f"https://opentdb.com/api.php?amount={amount}&category={category}&difficulty={difficulty}&type={question_type}"
+    response = requests.get(url)
+    data = response.json()
+
+    questions = []
+    options = []
+    answer = []
+
+    for question in data['results']:
+        questions.append(question['question'])
+        incorrect_answers = question['incorrect_answers']
+        correct_answer = question['correct_answer']
+        all_answers = incorrect_answers + [correct_answer]
+        shuffle(all_answers)
+        options.append(all_answers)
+        answer.append(all_answers.index(correct_answer) + 1)
+
+    quiz_data = {
+        'question': questions,
+        'options': options,
+        'answer': answer
+    }
+
+    with open('data.json', 'w') as file:
+        json.dump(quiz_data, file, indent=4)
+
+class StartWindow:
+    def __init__(self):
+        self.start_gui()
+
+    def start_gui(self):
+        self.start_window = Tk()
+        self.start_window.geometry("300x200")
+        self.start_window.title("Quiz Start")
+
+        category_label = Label(self.start_window, text="Select Category:")
+        category_label.pack()
+
+        self.category_var = StringVar(self.start_window)
+        self.category_var.set(category_options[0])
+        category_dropdown = OptionMenu(self.start_window, self.category_var, *category_options)
+        category_dropdown.pack()
+
+        difficulty_label = Label(self.start_window, text="Select Difficulty:")
+        difficulty_label.pack()
+
+        self.difficulty_var = StringVar(self.start_window)
+        self.difficulty_var.set(difficulty_options[0])
+        difficulty_dropdown = OptionMenu(self.start_window, self.difficulty_var, *difficulty_options)
+        difficulty_dropdown.pack()
+
+        amount_label = Label(self.start_window, text="Enter Number of Questions: (Preferably < 20)")
+        amount_label.pack()
+
+        self.amount_entry = Entry(self.start_window)
+        self.amount_entry.pack()
+
+        start_button = Button(self.start_window, text="Start", command=self.start_quiz)
+        start_button.pack()
+
+        self.start_window.mainloop()
+
+    def start_quiz(self):
+        category = category_options.index(self.category_var.get()) + 9
+        difficulty = self.difficulty_var.get()
+        amount = int(self.amount_entry.get())
+
+        fetch_questions(amount=amount, category=category, difficulty=difficulty.lower(), question_type='multiple')
+
+        self.start_window.destroy()
+
+category_options = [
+    "General Knowledge",
+    "Entertainment: Books",
+    "Entertainment: Film",
+    "Entertainment: Music",
+    "Entertainment: Musicals & Theatres",
+    "Entertainment: Television",
+    "Entertainment: Video Games",
+    "Entertainment: Board Games",
+    "Science & Nature",
+    "Science: Computers",
+    "Science: Mathematics",
+    "Mythology",
+    "Sports",
+    "Geography",
+    "History",
+    "Politics",
+    "Art",
+    "Celebrities",
+    "Animals",
+    "Vehicles",
+    "Entertainment: Comics",
+    "Science: Gadgets",
+    "Entertainment: Japanese Anime & Manga",
+    "Entertainment: Cartoon & Animations"
+]
+
+difficulty_options = ["Easy", "Medium", "Hard"]
+
+StartWindow()
+
+class GUI:
+    def __init__(self):
+        self.question_number = 0
+        self.display_title()
+        
+        self.option_selected = IntVar()
+        self.options = self.radio_buttons()
+        self.display_question()
+        self.display_options()
+        
+        self.buttons()
+        self.total_questions = len(question)
+        self.correct = 0
+        
+    def display_result(self):
+        correct = f"Correct: {self.correct}"
+        incorrect = f"Incorrect: {self.total_questions - self.correct}"
+        
+        score = int(self.correct / self.total_questions * 100)
+        result = f"Score: {score}%"
+        
+        mb.showinfo("Result", f"{result}\n{correct}\n{incorrect}")
+        
+    def check_answer(self, question_number):
+        if self.option_selected.get() == answer[question_number]:
+            return True
+        
+    def next_button(self):
+        if self.check_answer(self.question_number):
+            engine.say("Correct!")
+            engine.runAndWait()
+            self.correct += 1
+        else:
+            engine.say("Incorrect!")
+            engine.runAndWait()
+            
+        self.question_number += 1
+        
+        if self.question_number == self.total_questions:
+            self.display_result()
+            gui.destroy()
+            
+        else:
+            self.display_question()
+            self.display_options()
+            
+    def buttons(self):
+        button_frame = Frame(gui)
+        button_frame.pack(side=BOTTOM, pady=10)
+        
+        next_button = Button(button_frame, text="Next", command=self.next_button)
+        next_button.pack(side=LEFT, padx=20)
+        
+        quit_button = Button(button_frame, text="Quit", command=gui.destroy)
+        quit_button.pack(side=RIGHT, padx=20)
+        
+    def display_options(self):
+        val = 0
+        self.option_selected.set(0)
+        
+        for option, radio_btn in zip(options[self.question_number], self.options):
+            radio_btn.configure(text=html.unescape(option))
+            val += 1
+    
+    def display_question(self):
+        for widget in gui.winfo_children():
+            if isinstance(widget, Label):
+                widget.destroy()
+        
+        question_label = Label(gui, text=html.unescape(question[self.question_number]))
+        question_label.pack(side=TOP)
+        
+        
+    def display_title(self):
+        title = Label(gui, text="Quiz")
+        title.pack(side=TOP)
+        
+    def radio_buttons(self):
+        q_list = []
+        
+        y_pos = 50
+        
+        while len(q_list) < 4:
+            radio_btn = Radiobutton(gui, text=" ", variable=self.option_selected, value=len(q_list) + 1)
+            q_list.append(radio_btn)
+            radio_btn.pack(side=TOP, ipady=5)
+            radio_btn.place(x=100, y=y_pos)
+            y_pos += 25
+            
+        return q_list
+    
+gui = Tk()
+gui.geometry("750x250")
+gui.title("Quiz")
+
+with open('data.json') as f:
+    data = json.load(f)
+    
+question = (data['question'])
+options = (data['options'])
+answer = (data['answer'])
+
+quiz = GUI()
+gui.mainloop()
